bda501da0e01de61f121b36cf9fe4183ff307c53
Bakeloh <bakelohnoel@gmail.com>
2023-11-01 08:23:17 +0300

    Event Listener

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-let imgBox = document.getElementById("imgBox");
-let qrImage = document.getElementById("QrImage");
 
-function generateQR(){
+document.addEventListener("DOMContentLoaded", function generateQR() {
+    if (qrText.value.length > 0) {
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+        imgBox.classList.add("show-img");
+    }
 
-}
+
+
+
+    let imgBox = document.getElementById("imgBox");
+    let qrImage = document.getElementById("qrImage");
+    let qrText = document.getElementById("qrText");
+
+    // function generateQR(){
+    //     qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+
+    // 00009.36*60
+    imgBox.classList.add("show-img");
+    // comment on above
+});
