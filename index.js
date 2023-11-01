1b764f877cebb7ae33dd423bb0989dbff058a3ce
Bakeloh <bakelohnoel@gmail.com>
2023-11-01 16:24:40 +0300

    Global scope defined

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
 
-document.addEventListener("DOMContentLoaded", function generateQR() {
-    if (qrText.value.length > 0) {
-        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
-        imgBox.classList.add("show-img");
-    }else{
-        qrText.classList.add(`error`);
-        setTimeout(()=>{
-            qrText.classList.remove(`error`);
-        },1000);
-    }
-
-
-
-
-    let imgBox = document.getElementById("imgBox");
-    let qrImage = document.getElementById("qrImage");
+function generateQR() {
     let qrText = document.getElementById("qrText");
+    let qrImage = document.getElementById("qrImage");
+    let imgBox = document.getElementById("imgBox");
 
-    // function generateQR(){
-    //     qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
-
-    // 00009.36*60
-    imgBox.classList.add("show-img");
-    // comment on above
-});
+    if (qrText.value.length > 0) {
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + encodeURIComponent(qrText.value);
+        imgBox.classList.add("show-img");
+    } else {
+        qrText.classList.add("error");
+        setTimeout(() => {
+            qrText.classList.remove("error");
+        }, 1000);
+    }
+}
